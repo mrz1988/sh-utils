5610a7467013b592ef269138871fe078e42715b4
mrz1988 <mrz2004@gmail.com>
2019-06-26 09:47:28 -0700

    Fix up wip-swap a bit

--- git/changed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CHANGES=$(git status --porcelain)
-if [ -n "$CHANGES" ];
-    then echo changed
-else
-  echo unchanged
-fi
--- git/wip-swap.sh
+++ git/wip-swap.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-git add -A
-git commit -m "@@WIP"
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+CHANGES=$(git status --porcelain)
+if [ -n "$CHANGES" ];
+then
+  echo "${YELLOW}Branch dirty, making temp commit...${NC}"
+  git add -A
+  git commit -m "@@WIP"
+fi
+
+echo "${GREEN}Checking out branch ${1}...${NC}"
 git checkout $1
 if git log --pretty=oneline --max-count 1 | grep --quiet @@WIP; then
-  echo Found dirty WIP changes, uncommitting...
+  echo "${YELLOW}Found dirty WIP changes, uncommitting...${NC}"
   git reset HEAD~
 else
-  echo Commit history clean, no WIP.
+  echo "${GREEN}Commit history clean, no WIP.${NC}"
 fi
+
+echo "${GREEN}All set!${NC}"
